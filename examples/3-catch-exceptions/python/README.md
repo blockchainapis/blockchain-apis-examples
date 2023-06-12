4170eb35d7af5c5b1122e7f2873d107b127a762e
Nicolas <nicolas.went@epita.fr>
2023-06-12 16:34:26 +0200

    [Exceptions] Finished catching exceptions example for async

--- examples/3-catch-exceptions/python/README.md
+++ examples/3-catch-exceptions/python/README.md
@@ -7,6 +7,17 @@ These exceptions can happen only in the following cases:
 - You make more request than allowed by your API key (will throw a [TooManyRequestsException](https://docs.blockchainapis.io/docs/python-sdk/exceptions/too-many-requests-exception))
 - Your API key is expired or not invalid. In this case, an [UnauthorizedException](https://docs.blockchainapis.io/docs/python-sdk/exceptions/unauthorized-exception) is thrown.
 
+## Output
+
+Here is the output that you should get after executing the files:
+
+```sh
+Blockchain with id: invalid_blockchain_id is not supported
+==========================================================
+Given API key: invalid_api_key is not a valid API key
+To fix this error, get an API key at: https://dashboard.blockchainapis.io/
+```
+
 ## To run the example
 
 ### Install the dependencies:
--- examples/3-catch-exceptions/python/catch_exceptions.py
+++ examples/3-catch-exceptions/python/catch_exceptions.py
@@ -16,7 +16,8 @@ async def handle_throw(blockchain_id: str, api_key: str | None = None):
             print(f"Blockchain with id: {blockchain_id} is not supported")
         except UnauthorizedException:
             # Catch in case the given API key is invalid
-            print(f"Given API key: {api_key} is not valid")
+            print(f"Given API key: {api_key} is not a valid API key")
+            print("To fix this error, get an API key at: https://dashboard.blockchainapis.io/")
         except BlockchainAPIsException as e:
             # Catch in case any exception is thrown from Blockchain APIs.
             # try to comment all of the except above and see what happens
@@ -26,4 +27,5 @@ async def handle_throw(blockchain_id: str, api_key: str | None = None):
             print(f"Got an exception that is not related to Blockchain APIs:", e)
 
 asyncio.run(handle_throw("invalid_blockchain_id"))
+print("==========================================================")
 asyncio.run(handle_throw("ethereum", "invalid_api_key"))
