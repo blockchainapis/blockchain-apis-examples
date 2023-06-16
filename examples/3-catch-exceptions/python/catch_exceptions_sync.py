9cbcea30983523d679599bd1eac2b7f3882e37ee
Nicolas <nicolas.went@epita.fr>
2023-06-16 13:38:42 +0200

    [CatchExceptions] Fixed exceptions

--- examples/3-catch-exceptions/python/catch_exceptions_sync.py
+++ examples/3-catch-exceptions/python/catch_exceptions_sync.py
@@ -4,6 +4,7 @@ from blockchainapis import BlockchainAPIsSync
 from blockchainapis.exceptions import BlockchainNotSupportedException
 from blockchainapis.exceptions import UnauthorizedException
 from blockchainapis.exceptions import BlockchainAPIsException
+from blockchainapis.exceptions import UnknownBlockchainAPIsException
 
 def handle_throw(blockchain_id: str, api_key: str | None = None):
     blockchain_apis = BlockchainAPIsSync(api_key)
@@ -16,13 +17,14 @@ def handle_throw(blockchain_id: str, api_key: str | None = None):
         # Catch in case the given API key is invalid
         print(f"Given API key: {api_key} is not a valid API key")
         print("To fix this error, get an API key at: https://dashboard.blockchainapis.io/")
+    except UnknownBlockchainAPIsException as e:
+        # Catch in case an unknown Exception is thrown, this should not happen.
+        print(f"An unknown exception has hapenned:")
+        print(e)
     except BlockchainAPIsException as e:
         # Catch in case any exception is thrown from Blockchain APIs.
         # try to comment all of the except above and see what happens
         print(f"Got an exception from the API:", e)
-    except Exception as e:
-        # Catch when an exceptions is thrown that is not related to the API
-        print(f"Got an exception that is not related to Blockchain APIs:", e)
-        
+
 handle_throw("invalid_blockchain_id")
 handle_throw("ethereum", "invalid_api_key")
